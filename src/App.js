76a1232604206fa40df8b85f3931836232da57fa
Alok Raj <alokr417@gmail.com>
2021-03-26 08:42:57 +0530

    🛠 UI setup done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import "./App.css";
+import Contacts from "./components/contacts/Contacts";
+import Navbar from "./components/elements/Navbar";
+import "./styles/App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <p>heyyy</p>
+    <div>
+      <div className="jumbotron text-center bg-primary text-white">
+        <h1>Contact Management</h1>
+        <p>MEAN App - It remembers Everything.</p>
+      </div>
+      <Navbar />
+      <div className="container">
+        <div className="py-3">
+          <Contacts />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/elements/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <div className="text-center">
+      <button type="button" class="btn btn-outline-primary">
+        ➕ Contact
+      </button>
+    </div>
+  );
+}
